dd14e9966cb8b77e3aee355e8ffd8343512e622a
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-07-13 15:10:00 -0300

    sec commit.

---
+++ config/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+async function connect() {
+  try {
+    const dbConnect = await mongoose.conect(process.env.MONGODB_URI);
+    console.log("DB connected to:", dbConnect.connection.name);
+  } catch (err) {
+    console.error("DB connection error", err);
+  }
+}
+module.exports = connect;
