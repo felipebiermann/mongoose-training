406f748a80412510a5a2069c9a462cb9a025d37a
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-07-13 15:34:09 -0300

    db config e models ok.

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    const dbConnect = await mongoose.conect(process.env.MONGODB_URI);
+    const dbConnect = await mongoose.connect(process.env.MONGODB_URI);
     console.log("DB connected to:", dbConnect.connection.name);
   } catch (err) {
     console.error("DB connection error", err);
---
+++ models/Game.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = require("mongoose");
+
+const gameSchema = new Schema({
+  userName: { type: String, required: true, trim: true },
+  login: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const GameModel = model("Game", gameSchema);
+
+module.exports = GameModel;
