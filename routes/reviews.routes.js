9d48952f72a30df32667e6ab481a28df63c92f78
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-07-14 17:09:30 -0300

    crud complete do review.

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const ReviewModel = require("../models/Review.model");
 
 const GameModel = require("../models/Game.model");
+
+//CREATE
 router.post("/create-review/:gameId", async (req, res) => {
   try {
     const { gameId } = req.params;
@@ -14,6 +16,65 @@ router.post("/create-review/:gameId", async (req, res) => {
     );
 
     return res.status(201).json({ newReview, editedGame });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+});
+
+//READ
+router.get("/all-review", async (req, res) => {
+  try {
+    const allReview = await ReviewModel.find();
+
+    return res.status(200).json(allReview);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+});
+
+//READ DETAILS
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const detailReview = await ReviewModel.findOne({ _id: id }).populate(
+      "reviews"
+    );
+    return res.status(200).json(detailReview);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+});
+
+//EDIT
+
+router.patch("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const editedReview = await ReviewModel.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true }
+    );
+
+    return res.status(200).json(editedReview);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+});
+
+//DELETE
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedReview = await ReviewModel.deleteOne({ _id: id });
+
+    return res.status(200).json(deletedReview);
   } catch (err) {
     console.error(err);
     return res.status(500).json(err);
